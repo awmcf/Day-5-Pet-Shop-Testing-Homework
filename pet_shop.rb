179605c9360852a0516c4467736ac76ba0685856
Aaron McFaull <Aaron@Aarons-MacBook-Pro.local>
2019-01-20 20:48:52 +0000

    Test 6 increase pets sold solved

--- pet_shop.rb
+++ pet_shop.rb
@@ -14,8 +14,8 @@ def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
 
-def increase_pets_sold
-
+def increase_pets_sold(pet_shop, new_pets_sold)
+  return pet_shop[:admin][:pets_sold] += new_pets_sold
 end
 
 def stock_count(pet_shop)
